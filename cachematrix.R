a4b732f8af77f2a56b0f4fea943c1d87104e79a8
AnnaPerelman <AnnaPerelman@users.noreply.github.com>
2014-07-25 13:18:32 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -8,15 +8,15 @@
 ## makeCacheMatrix: This function creates
 ## a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-        inv<-NULL
-        set<-function(y){
+        inv<-NULL ## assigns NULL value to inv, where the inverted matrix will be stored in future
+        set<-function(y){ ## this function replaces x by y and assigns NULL to inv
                 x <<- y
                 inv <<- NULL
         }
-        get<-function()x
-        setinverse<-function(inverse) inv<<-inverse
-        getinverse<-function() inv
-        list(set = set, get = get,
+        get<-function()x ## this function returns X
+        setinverse<-function(inverse) inv<<-inverse ## this function assigns inverse value to inv
+        getinverse<-function() inv ## this function returns inv
+        list(set = set, get = get, ## the entire function returns a list of all 4 above funtions
              setinverse = setinverse,
              getinverse = getinverse)
 
@@ -28,15 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## If the inverse has already been calculated (and the matrix has not changed),
 ## then the cachesolve should retrieve the inverse from the cache. 
 
-cacheSolve <- function(x, ...) {
-        inv <- x$getinverse()
-        if(!is.null(inv)) {
+cacheSolve <- function(x, ...) { ## receives a matrix creadted by the previous function
+        inv <- x$getinverse() ## assigns to x the output of getinverse funtion out of the list x
+        if(!is.null(inv)) { ## checks if the matrix was inverted and saved before
                 message("getting cached data")
                 return(inv)
         }
-        data <- x$get()
-        inv <- solve(data, ...)
-        x$setinverse(inv)
-        inv
+        data <- x$get() ## if not assigns its value to data
+        inv <- solve(data, ...) ## performs the inverse and assigns the result to inv
+        x$setinverse(inv) ## calls the setinverse funtion
+        inv ## returns inv
         
 }
